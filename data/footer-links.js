7e0c3c3d41ecfcfc3b235cc80866bbd553661624
iamsuhail <s.muhammadsuhail123@gmail.com>
2024-08-07 13:55:03 +0530

    pushing frontend to development stage

---
+++ data/footer-links.js
@@ -0,0 +1,52 @@
+export const FooterLink2 = [
+  {
+    title: "Subjects",
+    links: [
+      { title: "Java Basics and OOPS", link: "/java" },
+      { title: "DSA Foundations", link: "/code-foundations" },
+      { title: "DSA Advanced", link: "/dsa-adv" },
+      { title: "Interview preparation", link: "/preparation" },
+      { title: "Web Development", link: "/web-development" },
+      { title: "Mobile Development", link: "/mobile-development" },
+      // { title: "Computer Science", link: "/computer-science" },
+      // { title: "Cybersecurity", link: "/cybersecurity" },
+      // { title: "Data Analytics", link: "/data-analytics" },
+      // { title: "Data Science", link: "/data-science" },
+      { title: "Cloud Computing", link: "/cloud-computing" },
+      { title: "DevOps", link: "/devops" },
+      // { title: "Game Development", link: "/game-development" },
+      { title: "Machine Learning", link: "/machine-learning" },
+      // { title: "Web Design", link: "/web-design" },
+    ],
+  },
+  {
+    title: "Languages",
+    links: [
+      // { title: "Bash", link: "/bash" },
+      // { title: "C++", link: "/c++" },
+      { title: "C#", link: "/csharp" },
+      // { title: "Go", link: "/go" },
+      { title: "HTML & CSS", link: "/html-css" },
+      { title: "Java", link: "/java" },
+      { title: "JavaScript", link: "/javascript" },
+      // { title: "Kotlin", link: "/kotlin" },
+      // { title: "PHP", link: "/php" },
+      { title: "Python", link: "/python" },
+      // { title: "R", link: "/r" },
+      // { title: "Ruby", link: "/ruby" },
+      { title: "SQL", link: "/sql" },
+      // { title: "Swift", link: "/swift" },
+    ],
+  },
+  {
+    title: "Career building",
+    links: [
+        {title: "Career paths", link: "/career-paths"},
+        {title: "Career services", link: "/career-services"},
+        {title: "Interview prep", link: "/interview-prep"},
+        {title: "Professional certification ", link: "/professional-certification"},
+        {title: "Full Catalog", link: "/full-catalog"},
+        {title: "Beta Content", link: "/beta-content"}
+    ]
+  }
+];
